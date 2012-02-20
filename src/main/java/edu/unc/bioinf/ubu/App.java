24ed1abbc1c3f27ac9426f836cfa6ce3bb0a6ab1
Lisle Mose <lislemose@gmail.com>
2012-02-20 17:09:26 -0500

    Added src dir

---
+++ src/main/java/edu/unc/bioinf/ubu/App.java
@@ -0,0 +1,13 @@
+package edu.unc.bioinf.ubu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
