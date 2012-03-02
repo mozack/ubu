dd2c36c47e9b58d1dc449e3b71910134e21300ec
Lisle Mose <lislemose@gmail.com>
2012-03-02 13:18:30 -0500

    Removed unused include.

--- src/main/java/edu/unc/bioinf/ubu/sam/SpliceJunctionMap.java
+++ src/main/java/edu/unc/bioinf/ubu/sam/SpliceJunctionMap.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
