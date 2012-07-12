1ce15a1347224857b2525320c14d962b5e2be423
Lisle Mose <lislemose@gmail.com>
2012-07-12 11:36:25 -0400

    Reverting to pre-fusion version.

--- src/main/java/edu/unc/bioinf/ubu/fastq/Sam2Fastq.java
+++ src/main/java/edu/unc/bioinf/ubu/fastq/Sam2Fastq.java
@@ -2,12 +2,9 @@ package edu.unc.bioinf.ubu.fastq;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
-import edu.unc.bioinf.ubu.sam.ReadBlock;
 import edu.unc.bioinf.ubu.sam.ReverseComplementor;
 
-import net.sf.samtools.CigarOperator;
 import net.sf.samtools.SAMFileReader;
 import net.sf.samtools.SAMRecord;
 import net.sf.samtools.SAMFileReader.ValidationStringency;
@@ -25,15 +22,10 @@ public class Sam2Fastq {
 	private boolean shouldIdentifyEndByReadId = false;
 	private String end1Suffix;
 	private String end2Suffix;
-	private boolean shouldHandleMapspliceFusions;
-	
-	private static final String MAPSLICE_FUSION_TAG = "ZF";
-	private static final String MAPSLICE_FUSION     = "FUS";
 
 	/**
 	 * Convert the input paired end SAM/BAM file into 2 fastq files.
 	 * Input SAM files that contain multiple mappings should be sorted by read name.
-	 * Note: muti-mappers that do not contain /1 or /2 in the read name may not be handled properly due to sort order.
 	 */
 	public void convert(String inputSam, String outputFastq1, String outputFastq2) throws IOException {
 		String last1Read = "";
@@ -51,50 +43,18 @@ public class Sam2Fastq {
         int output2Count = 0;
         int lineCnt = 0;
         
-        SAMRecord fusionRead = null;
-        
         for (SAMRecord read : reader) {
         	if (isFirstInPair(read)) {
         		if (!read.getReadName().equals(last1Read)) {
-                	if (fusionRead != null) {
-                		System.out.println("UNMATCHED FUSION READ: " + fusionRead.getSAMString());
-                		fusionRead = null;
-                	}
+        			output1.write(samReadToFastqRecord(read));
         			last1Read = read.getReadName();
-        			if (!isFusion(read)) {
-	        			output1.write(samReadToFastqRecord(read));
-	        			output1Count += 1;
-	        			fusionRead = null;
-        			} else {
-        				fusionRead = read;
-        			}
-        		} else {
-        			if (isMatchingFusionRead(fusionRead, read)) {
-        				output1.write(fusionToFastqRecord(fusionRead, read));
-        				output1Count += 1;
-        				fusionRead = null;
-        			}
+        			output1Count += 1;
         		}
         	} else if (isSecondInPair(read)) {
         		if (!read.getReadName().equals(last2Read)) {
-                	if (fusionRead != null) {
-                		System.out.println("UNMATCHED FUSION READ: " + fusionRead.getSAMString());
-                		fusionRead = null;
-                	}
+        			output2.write(samReadToFastqRecord(read));
         			last2Read = read.getReadName();
-        			if (!isFusion(read)) {
-	        			output2.write(samReadToFastqRecord(read));
-	        			output2Count += 1;
-	        			fusionRead = null;
-        			} else {
-        				fusionRead = read;
-        			}
-        		} else {
-        			if (isMatchingFusionRead(fusionRead, read)) {
-        				output2.write(fusionToFastqRecord(fusionRead, read));
-        				output2Count += 1;
-        				fusionRead = null;
-        			}        			
+        			output2Count += 1;
         		}
         	}
         	
@@ -110,51 +70,6 @@ public class Sam2Fastq {
         if (output1Count != output2Count) {
         	throw new IllegalStateException("Non-symmetrical read counts found for " + inputSam + ".  Your reads may not be paired properly.");
         }
-	}
-	
-	private boolean isMatchingFusionBlocks(SAMRecord read1, SAMRecord read2) {
-		boolean isMatching = false;
-		
-		List<ReadBlock> read1Blocks = ReadBlock.getReadBlocks(read1);
-		List<ReadBlock> read2Blocks = ReadBlock.getReadBlocks(read2);
-		
-		ReadBlock read1FirstBlock = read1Blocks.get(0);
-		ReadBlock read1LastBlock  = read1Blocks.get(read1Blocks.size()-1);
-		ReadBlock read2FirstBlock = read2Blocks.get(0);
-		ReadBlock read2LastBlock  = read2Blocks.get(read2Blocks.size()-1);
-		
-		if ((read1FirstBlock.getType() == CigarOperator.S) &&
-			(read2LastBlock.getType() == CigarOperator.S) &&
-			(read1FirstBlock.getLength() + read2LastBlock.getLength() == read1.getReadLength())) {
-			isMatching = true;
-		} else if ((read1LastBlock.getType() == CigarOperator.S) &&
-				   (read2FirstBlock.getType() == CigarOperator.S) &&
-				   (read1LastBlock.getLength() + read2FirstBlock.getLength() == read1.getReadLength())) {
-			isMatching = true;
-		}
-		
-		return isMatching;
-	}
-	
-	private boolean isMatchingFusionRead(SAMRecord fusionRead, SAMRecord read) {
-		return (
-				(fusionRead != null) &&
-				(isFusion(read)) && 
-				(read.getAttribute(MAPSLICE_FUSION_TAG).equals(fusionRead.getAttribute(MAPSLICE_FUSION_TAG))) &&
-				(read.getReadString().equals(fusionRead.getReadString())) &&
-				(isMatchingFusionBlocks(fusionRead, read))
-				);
-	}
-	
-	private boolean isFusion(SAMRecord read) {
-		boolean isFusion = false;
-		
-		if (shouldHandleMapspliceFusions) {
-			String fusionTag = (String) read.getAttribute(MAPSLICE_FUSION_TAG);
-			isFusion = (fusionTag != null) && (fusionTag.contains(MAPSLICE_FUSION));
-		}
-		
-		return isFusion;
 	}
 	
 	/**
@@ -200,69 +115,6 @@ public class Sam2Fastq {
 		return fastq;
 	}
 	
-	private FastqRecord fusionToFastqRecord(SAMRecord read1, SAMRecord read2) {
-		
-		SAMRecord doner = null;
-		SAMRecord accepter = null;
-		int donerLength = -1;
-		int accepterStart = -1;
-		
-		List<ReadBlock> read1Blocks = ReadBlock.getReadBlocks(read1);
-		List<ReadBlock> read2Blocks = ReadBlock.getReadBlocks(read2);
-		
-		ReadBlock read1FirstBlock = read1Blocks.get(0);
-		ReadBlock read1LastBlock  = read1Blocks.get(read1Blocks.size()-1);
-		ReadBlock read2FirstBlock = read2Blocks.get(0);
-		ReadBlock read2LastBlock  = read2Blocks.get(read2Blocks.size()-1);
-		
-		if ((read1FirstBlock.getType() == CigarOperator.S) &&
-			(read2LastBlock.getType() == CigarOperator.S)) {
-			
-			accepter = read1;
-			accepterStart = read1FirstBlock.getLength() + 1;
-
-			doner = read2;
-			donerLength = read2.getReadLength() - read2LastBlock.getLength();
-			
-		} else if ((read1LastBlock.getType() == CigarOperator.S) &&
-				   (read2FirstBlock.getType() == CigarOperator.S)) {
-			
-			doner = read1;
-			donerLength = read1.getReadLength() - read1LastBlock.getLength();
-			
-			accepter = read2;
-			accepterStart = read2FirstBlock.getLength();
-		} else {
-			throw new IllegalArgumentException("Invalid fusion detected. read1: " + read1.getSAMString() +
-					" read2: " + read2.getSAMString());
-		}
-		
-		String donerBases = doner.getReadString().substring(0, donerLength);
-		if (doner.getReadNegativeStrandFlag()) {
-			donerBases = reverseComplementor.reverseComplement(donerBases);
-		}
-		
-		String accepterBases = accepter.getReadString().substring(accepterStart);
-		if (accepter.getReadNegativeStrandFlag()) {
-			accepterBases = reverseComplementor.reverseComplement(accepterBases);
-		}
-		
-		String bases = donerBases + accepterBases;
-		
-		// Assumption: Qualities are in the original format
-		// TODO: May not be true.
-		String qualities = doner.getBaseQualityString();
-		
-		// Read name should be same for both reads.
-		String readName = doner.getReadName();
-		
-		FastqRecord fastq = new FastqRecord("@" + readName, bases, qualities);
-		
-		System.out.println("Fusion: " + readName);
-		
-		return fastq;
-	}
-	
 	private boolean isFirstInPair(SAMRecord read) {
 		boolean isFirstInPair;
 		
@@ -295,10 +147,6 @@ public class Sam2Fastq {
 		this.end2Suffix = end2Suffix;
 	}
 	
-	public void setShouldHandleMapspliceFusions(boolean shouldHandleMapspliceFusions) {
-		this.shouldHandleMapspliceFusions = shouldHandleMapspliceFusions;
-	}
-	
 	public static void run(String[] args) throws IOException {
 		Sam2FastqOptions options = new Sam2FastqOptions();
 		options.parseOptions(args);
@@ -308,8 +156,6 @@ public class Sam2Fastq {
 			System.out.println("sam2fastq starting");
 			
 			Sam2Fastq sam2Fastq = new Sam2Fastq();
-			sam2Fastq.setShouldHandleMapspliceFusions(options.shouldHandleMapspliceFusions());
-			
 			if (options.isPairedEnd()) {
 				
 				if (options.shouldIdEndByReadName()) {
@@ -318,20 +164,11 @@ public class Sam2Fastq {
 				
 				sam2Fastq.convert(options.getInputFile(), options.getFastq1(), options.getFastq2());
 			} else {
-				//throw new IllegalArgumentException("Single end is not currently supported");
-				System.out.println("WARNING: Single end not thoroughly tested!\n");
 				sam2Fastq.convert(options.getInputFile(), options.getFastq1());
 			}
 			
 			long e = System.currentTimeMillis();
 			System.out.println("sam2fastq done.  Elapsed secs: " + (e-s)/1000);
 		}
-	}
-	
-	public static void main(String[] args) throws Exception {
-		String[] argz =
-			"--in /home/lisle/sam2fastq/fusion3.sam --fastq1 /home/lisle/sam2fastq/fus3_1.fastq --fastq2 /home/lisle/sam2fastq/fus3_2.fastq --end1 /1 --end2 /2 --mfusion".split(" ");
-		
-		run(argz);
 	}
 }
